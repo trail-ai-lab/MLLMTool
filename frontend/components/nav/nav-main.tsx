d7188546f03cb055adc5c86ec028f9898f4ad80a
Anurag Maravi <amaravi@dataprisms.io>
2025-07-27 09:05:33 -0500

    Add audio recorder and save audio to GCS

--- frontend/components/nav/nav-main.tsx
+++ frontend/components/nav/nav-main.tsx
@@ -10,10 +10,13 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import { AddSourceDialog } from "@/components/dialogs/add-source-dialog"
+import { RecordAudioDialog } from "@/components/dialogs/record-audio-dialog"
 import { useState } from "react"
 
 export function NavMain({}: {}) {
-  const [open, setOpen] = useState(false)
+  const [openUpload, setOpenUpload] = useState(false)
+  const [openRecord, setOpenRecord] = useState(false)
+
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
@@ -21,6 +24,7 @@ export function NavMain({}: {}) {
           <SidebarMenuItem className="flex items-center gap-2">
             <SidebarMenuButton
               tooltip="Record Audio"
+              onClick={() => setOpenRecord(true)}
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
             >
               <IconPlayerRecord stroke={2} />
@@ -30,13 +34,20 @@ export function NavMain({}: {}) {
           <SidebarMenuItem className="flex items-center gap-2">
             <SidebarMenuButton
               tooltip="Add Source"
-              onClick={() => setOpen(true)}
+              onClick={() => setOpenUpload(true)}
             >
               <IconUpload />
               <span>Add Source</span>
             </SidebarMenuButton>
           </SidebarMenuItem>
-          <AddSourceDialog open={open} onClose={() => setOpen(false)} />
+          <AddSourceDialog
+            open={openUpload}
+            onClose={() => setOpenUpload(false)}
+          />
+          <RecordAudioDialog
+            open={openRecord}
+            onClose={() => setOpenRecord(false)}
+          />{" "}
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
